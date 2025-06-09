372e51581131cc5300a62fbcad2b1dce9ade9ed3
Vandy241996 <vandy241996@gmail.com>
2025-06-09 22:36:08 +0530

    modifications in yml

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.Test Vandy
+            Save and see your changes instantly.Test Vandy here!
           </li>
         </ol>
 
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our docs Test
           </a>
         </div>
       </main>
