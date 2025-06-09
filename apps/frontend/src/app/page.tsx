b056df89c73c2c1f2aa5ae18a99ed1a9d12c26f7
Vandy241996 <vandy241996@gmail.com>
2025-06-09 23:28:12 +0530

    testing ci/cd pipeline

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.Test Vandy here!
+            Save and see your changes instantly.Test Vandy Hello!
           </li>
         </ol>
 
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs Test
+            Read our docs
           </a>
         </div>
       </main>
