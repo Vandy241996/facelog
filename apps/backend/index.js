84d0a012de598187f5dfec2cc6a21903dd1f452c
Vandy241996 <your_email_on_github@domain.com>
2025-06-11 23:48:14 +0530

    fix db connection

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -15,22 +15,22 @@ const pool = new Pool({
   },
 });
 
-pool
-  .connect()
-  .then(() => console.log("✅ Connected to PostgreSQL"))
-  .catch((err) => {
-    console.error("❌ Failed to connect to PostgreSQL:", err); // full error
-    process.exit(1);
-  });
+// pool
+//   .connect()
+//   .then(() => console.log("✅ Connected to PostgreSQL"))
+//   .catch((err) => {
+//     console.error("❌ Failed to connect to PostgreSQL:", err); // full error
+//     process.exit(1);
+//   });
 
-app.get("/", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT NOW()");
-    res.send(`Hello from PostgreSQL! Time is ${result.rows[0].now}`);
-  } catch (err) {
-    res.status(500).send("DB error: " + err.message);
-  }
-});
+// app.get("/", async (req, res) => {
+//   try {
+//     const result = await pool.query("SELECT NOW()");
+//     res.send(`Hello from PostgreSQL! Time is ${result.rows[0].now}`);
+//   } catch (err) {
+//     res.status(500).send("DB error: " + err.message);
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
